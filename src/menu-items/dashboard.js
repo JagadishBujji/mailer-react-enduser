f0304c5cb06c5ecbf61a41e842c9f6d2bf463dff
Gopi8939 <gopiaswin2002@gmail.com>
2022-07-26 16:22:04 +0530

    mailer ui

---
+++ src/menu-items/dashboard.js
@@ -0,0 +1,89 @@
+// assets
+import {
+    DashboardOutlined,
+    UserOutlined,
+    HistoryOutlined,
+    AppstoreAddOutlined,
+    ImportOutlined,
+    UsergroupAddOutlined,
+    WindowsOutlined
+} from '@ant-design/icons';
+
+// icons
+const icons = {
+    DashboardOutlined,
+    UserOutlined,
+    HistoryOutlined,
+    AppstoreAddOutlined,
+    ImportOutlined,
+    UsergroupAddOutlined,
+    WindowsOutlined
+};
+
+// ==============================|| MENU ITEMS - DASHBOARD ||============================== //
+
+const dashboard = {
+    id: 'group-dashboard',
+    title: 'Navigation',
+    type: 'group',
+    children: [
+        {
+            id: 'dashboard',
+            title: 'Dashboard',
+            type: 'item',
+            url: '/dashboard/default',
+            icon: icons.DashboardOutlined,
+            breadcrumbs: false
+        },
+        {
+            id: 'user',
+            title: 'Users',
+            type: 'item',
+            url: '/user/default',
+            icon: icons.UsergroupAddOutlined,
+            breadcrumbs: false
+        },
+        {
+            id: 'batches',
+            title: 'Batches',
+            type: 'item',
+            url: '/batches/default',
+            icon: icons.AppstoreAddOutlined,
+            breadcrumbs: false
+        },
+        {
+            id: 'templates',
+            title: 'Templates',
+            type: 'item',
+            url: '/templates/default',
+            icon: icons.WindowsOutlined,
+            breadcrumbs: false
+        },
+        {
+            id: 'history&report',
+            title: 'history & Report',
+            type: 'item',
+            url: '/history&report/default',
+            icon: icons.HistoryOutlined,
+            breadcrumbs: false
+        },
+        {
+            id: 'imports',
+            title: 'Imports',
+            type: 'item',
+            url: '/imports/default',
+            icon: icons.ImportOutlined,
+            breadcrumbs: false
+        },
+        {
+            id: 'profile',
+            title: 'Profile',
+            type: 'item',
+            url: '/profile/default',
+            icon: icons.UserOutlined,
+            breadcrumbs: false
+        }
+    ]
+};
+
+export default dashboard;
